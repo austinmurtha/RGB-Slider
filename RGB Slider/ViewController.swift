d501d9de2673bf8054a3b92df873393d805036a0
Austin Murtha <Austin.Murtha@gmail.com>
2015-03-25 11:48:26 -0400

    Added navigation controller and sliders

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var boxView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(boxView)
+        //boxView.backgroundColor = UIColor.blackColor()
+        boxView.layer.borderColor = UIColor.blackColor().CGColor
+        boxView.layer.borderWidth = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue  = CGFloat(blueSlider.value)
+        
+        boxView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    
+    
+    }
 
     //Test comment for git
 }
